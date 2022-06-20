8670bf7e0cfbdfb14442293d08a8510195850525
Exis <gotexis@gmail.com>
2022-06-20 12:51:06 +1000

    node 16 / react bump

--- react_ws_src/devServer.js
+++ react_ws_src/devServer.js
@@ -26,10 +26,10 @@ app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-app.listen(3000, '0.0.0.0', function(err) {
+app.listen(3000, 'localhost', function(err) {
 	if (err) {
 		console.log(err);
 		return;
 	}
-	console.log('Listening at http://0.0.0.0:3000');
+	console.log('Listening at http://localhost:3000');
 });
